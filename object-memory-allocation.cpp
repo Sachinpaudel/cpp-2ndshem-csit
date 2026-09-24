e761a2b811f711bdc721fca3b19a02d57a0605b8
Sachin Paudel <sachinpaudel32@gmail.com>
2026-09-24 22:09:08 +0545

    new

--- object-memory-allocation.cpp
+++ object-memory-allocation.cpp
@@ -1,8 +1,28 @@
 // --------------------------Base model of object memory allocation in C++
+// class Employee
+// {
+// public:
+//     static int count; // returns number of employees
+//     string eName;
+
+//     void setName(string name)
+//     {
+//         eName = name;
+//         count++;
+//     }
+// };
+
+//     int Employee::count = 0; // defining the value of count
+
+
+
+#include <iostream>
+using namespace std;
+
 class Employee
 {
 public:
-    static int count; // returns number of employees
+    static int count; // static variable
     string eName;
 
     void setName(string name)
@@ -10,7 +30,19 @@ public:
         eName = name;
         count++;
     }
+
+    static int getCount() // static method
+    {
+        return count;
+    }
 };
 
-    int Employee::count = 0; // defining the value of count
+int Employee::count = 0; // defining the value of count
+
+int main()
+{
+    Employee Harry;
+    Harry.setName("Harry");
+    cout << Employee::getCount() << endl;
+}
 
